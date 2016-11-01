7f2e70e2f834a0a4e54e777b5c07b5f206edaf3c
ASTDB <nirmaljayasinghe@yahoo.com>
2016-11-01 20:27:00 +1100

    First working version commit formatted

--- DeviceNotRecordingAlarmFilter.go
+++ DeviceNotRecordingAlarmFilter.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
-    "bufio"
-    "log"
-    "strings"
+	"strings"
 )
 
 func main() {
@@ -26,18 +26,18 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-    r1, _ := regexp.Compile("[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]*:[0-9]{2} [A|P]M")
-    r2, _ := regexp.Compile("[0-9]{5}")
+	r1, _ := regexp.Compile("[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]*:[0-9]{2} [A|P]M")
+	r2, _ := regexp.Compile("[0-9]{5}")
 
 	for scanner.Scan() {
 		// fmt.Println(scanner.Text())
-        line := scanner.Text()
-        date := strings.TrimSpace(r1.FindString(line))
-        ext := strings.TrimSpace(r2.FindString(line))
+		line := scanner.Text()
+		date := strings.TrimSpace(r1.FindString(line))
+		ext := strings.TrimSpace(r2.FindString(line))
 
-        if len(date) > 0 && len(ext) > 0 {
-            fmt.Printf("%s\t%s\n", r1.FindString(line), r2.FindString(line))
-        }
+		if len(date) > 0 && len(ext) > 0 {
+			fmt.Printf("%s\t%s\n", r1.FindString(line), r2.FindString(line))
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
