8a466fc39b4bc7cdb7e0edbfd7c865c6fba6fd70
ASTDB <nirmaljayasinghe@yahoo.com>
2016-11-01 20:34:12 +1100

    First working version commit - add licence

--- DeviceNotRecordingAlarmFilter.go
+++ DeviceNotRecordingAlarmFilter.go
@@ -1,3 +1,5 @@
+// reads in a text file containing alarm traces (not provided) and prints out the date and extension portions for each alarm.
+
 package main
 
 import (
@@ -26,7 +28,10 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+    // detector for date and time e.g. "28/09/2016 9:00 AM"
 	r1, _ := regexp.Compile("[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]*:[0-9]{2} [A|P]M")
+
+    // detector for extension e.g. "34331"
 	r2, _ := regexp.Compile("[0-9]{5}")
 
 	for scanner.Scan() {
